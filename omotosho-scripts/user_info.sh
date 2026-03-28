92b989170a710d5b0ff9bc205117d801c659b7bf
jd.omotosho <jd.omotosho@gamil.com>
2026-03-28 17:36:55 +0100

    feat: omotosho-scripts

---
+++ omotosho-scripts/user_info.sh
@@ -0,0 +1,11 @@
+My Scripts 
+
+Summary Bash Command
+#cd bash-assignment		Enter the project folder
+#mkdir omotosho=log		Create a folder
+#cd scripts 			Enter the  script folder
+ls				See what is inside the folder
+nano user_info.sh		Create the script fileX
+
+
+I seems to be having issues saving this scripts

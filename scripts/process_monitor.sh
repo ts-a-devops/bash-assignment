21c26b452bd6f08c772faf90cdd241274255295d
root <root@lumzman.localdomain>
2026-04-01 12:10:06 +0100

    bash assignment

---
+++ scripts/process_monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+LOG_FILE="../logs/process_monitor.log"
+services=("nginx" "ssh" "docker")
+
+for service in "${services[@]}"; do
+  if pgrep -x "$service" > /dev/null; then
+ echo "$service is Running" | tee -a "$LOG_FILE"
+ else
+ echo "$service is Stopped. Restarting..." | tee -a "$LOG_FILE"
+  # Simulate restart
+ echo "$service Restarted" | tee -a "$LOG_FILE"
+     fi
+ done

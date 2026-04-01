df2664ab17b2e5a524cbec55bf7004724bb2d3c4
EniiyiH <eniiyiheritage@gmail.com>
2026-04-01 05:24:30 +0100

    Created new directory for easy navigation.

--- scripts/system_check.sh
+++ scripts/system_check.sh
@@ -2,20 +2,23 @@
 
  date=$(date +"%b_%Y")
 
-#CREATE log file 
- LOG_FILE="/home/eniiyi/bash-assignment/scripts/logs/system_report_${date}.log"
+#CREATE log dir
+LOG_DIR="/home/eniiyi/bash-assignment/scripts/logs/system_log"
+mkdir -p "$LOG_DIR"
+
+logs="$LOG_DIR/system_report_${date}.log"
 
  #output disk usage
- echo "=======Disk usage==========" | tee -a $LOG_FILE
- df -h | tee -a $LOG_FILE
+ echo "=======Disk usage==========" | tee -a "$logs"
+ df -h | tee -a "$logs"
 
  #output memory usage
- echo "=======Memory usage========" | tee -a $LOG_FILE
- free -m | tee -a $LOG_FILE
+ echo "=======Memory usage========" | tee -a "$logs"
+ free -m | tee -a "$logs"
 
  #output system uptime
- echo "=======System uptime=======" | tee -a $LOG_FILE
- { uptime -p; echo "${date}"; } | tee -a $LOG_FILE
+ echo "=======System uptime=======" | tee -a "$logs"
+ { uptime -p; echo "${date}"; } | tee -a "$logs"
 
 
 # Loop through each filesystem usage percent
@@ -23,7 +26,7 @@ df -h | awk 'NR>1 {print $1, $5}'| while read fs percent; do
     usage=${percent%\%}   # strip the % sign
     
     if [[ "$usage" -gt 80 ]]; then
-        echo "WARNING: $fs is at ${percent} usage!" | tee -a "$LOG_FILE"
+        echo "WARNING: $fs is at ${percent} usage!" | tee -a "$logs"
   	else exit 0 
     fi
 done
@@ -32,8 +35,8 @@ done
 echo "=======Total running processes========"
 actproc=$(ps aux | wc -l)
 
-echo "There are ${actproc} active processes" | tee -a "$LOG_FILE" 
+echo "There are ${actproc} active processes" | tee -a "$logs" 
 
 #sort top 5 memory consuming processes
-echo "====== top 5 mem consuming processes====="
-ps aux --sort=-%mem | awk '{print $1, $4}' |head -n5 | tee -a "$LOG_FILE"
+echo "====== top 5 mem consuming process====="
+ps aux --sort=-%mem | awk '{print $1, $4}' |head -n5 | tee -a "$logs"

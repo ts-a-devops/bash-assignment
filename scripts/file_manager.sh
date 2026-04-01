4d10d131436cf053ae91158885b53db60ae26852
arasheed <rashydodo@arasheed-NSP.localdomain>
2026-04-01 13:36:33 +0100

    feat: complete bash scripts

---
+++ scripts/file_manager.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+LOG_FILE="../logs/file_manager.log"
+ACTION=$1
+FILE=$2
+
+case "$ACTION" in
+            create)
+        if [[ -f "$FILE" ]]; then
+          echo "File already exists!" | tee -a "$LOG_FILE"
+        else
+          touch "$FILE"
+          echo "File created: $FILE" | tee -a "LOG_FILE"
+        fi
+       ;;
+       delete)
+
+     if [[ -f "$FILE" ]]; then
+        rm "$FILE"
+        echo "File deleted: $FILE" | tee -a "$LOG_FILE"
+       else
+       echo "File not found!" | tee -a "$LOG_FILE"
+     fi
+     ;;
+     list)
+     ls -lh | tee -a "LOG_FILE"
+     ;;
+     rename)
+      NEW_NAME=$3
+      if [[ -f "$FILE" ]]; then
+        mv "$FILE" "$NEW_NAME"
+        echo "Renamed $FILE to $NEW_NAME" | tee -a "$LOG_FILE"
+      else
+        echo "File not found!" | tee -a "$LOG_FILE"
+      fi
+      ;;
+      *)
+        echo "Usage: $0 {create|delete|list|rename} filename filename?" | tee -a "$LOG_FILE"
+      ;;
+
+esac
